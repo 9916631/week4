c39b6ec6a4d1429d45a2cbc310a08b3554a96933
stacey <ssmyth25@gmail.com>
2019-03-20 05:38:39 +1300

    i have no clue

---
+++ cw/cw/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            Console.WriteLine(num1);
+
+            Console.Write("what name?: ");
+            string name = Console.ReadLine();
+
+            Console.Write("what age?: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello {0}, you are {1} years old today",name,age);
+            Console.ReadLine();
+                
+
+            
+
+        }
+    }
+}
---
+++ johndoe/johndoe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace johndoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "hello world";
+            string name = "life is a girl dog";
+
+            Console.WriteLine(greeting);
+
+            Console.WriteLine("{0} {1}",greeting,name);
+            Console.ReadLine();
+        }
+    }
+}
